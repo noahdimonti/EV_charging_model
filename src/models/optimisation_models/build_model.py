76380b820111bc36147d7736207fc206787461a7
Noah Dimonti <noahdimonti@gmail.com>
2025-06-06 17:30:46 +1000

    add normalisation to objective, and refactor objective function implementation

--- src/models/optimisation_models/build_model.py
+++ src/models/optimisation_models/build_model.py
@@ -1,3 +1,5 @@
+import pandas as pd
+import os
 import pyomo.environ as pyo
 from src.config import params
 from src.models.utils.configs import (
@@ -16,12 +18,17 @@ class BuildModel:
     def __init__(self,
                  config: CPConfig,
                  charging_strategy: ChargingStrategy,
+                 version: str,
                  obj_weights: dict):
         self.config = config
         self.charging_strategy = charging_strategy
         self.obj_weights = obj_weights
 
-        self.model = pyo.ConcreteModel(name=f'{config.value}_{charging_strategy.value}_{params.num_of_evs}EVs')
+        self.f_star_values = {}
+
+        self.model = pyo.ConcreteModel(
+            name=f'{config.value}_{charging_strategy.value}_{params.num_of_evs}EVs_{version}'
+        )
         self.assets = {}
 
         # Validate configuration and charging mode
@@ -30,6 +37,7 @@ class BuildModel:
 
         # Run methods
         self.initialise_sets()
+        self.get_f_star()
         self.assemble_components()
 
     def initialise_sets(self):
@@ -37,6 +45,36 @@ class BuildModel:
         self.model.TIME = pyo.Set(initialize=params.timestamps)
         self.model.DAY = pyo.Set(initialize=[_ for _ in params.T_d.keys()])
         self.model.WEEK = pyo.Set(initialize=[_ for _ in params.D_w.keys()])
+
+    def get_f_star(self):
+        # Load precomputed f* values
+        obj_terms = [
+            'f_star_economic',
+            'f_star_technical',
+            'f_star_social'
+        ]
+
+        for term in obj_terms:
+            filename = f'{params.raw_val_metrics_filename_format}_{params.num_of_evs}EVs_{term}.csv'
+            filepath = os.path.join(params.f_star_values_folder_path, filename)
+            df = pd.read_csv(filepath, index_col=0)
+
+            # Extract the 'objective_value' row and transpose to become a column
+            extracted = df.loc[['objective_value']].transpose()
+            extracted.columns = [term]  # Rename the column to reflect the objective term
+
+            # Convert to dict with config as key and objective value as float, and assign to f_star_values attribute
+            _, _, term_name = term.split('_')  # convert term from 'f_star_name' into 'name'
+            self.f_star_values[term_name] = extracted[term].to_dict()
+
+    def _normalise_cost(self, cost, category):
+        model_name = f'{self.config.value}_{self.charging_strategy.value}'
+        f_star = self.f_star_values.get(category, {}).get(model_name, None)
+
+        if f_star in [None, 0]:  # prevent division by 0 or missing value
+            raise ValueError(f'Missing or zero f* value for `{category}` in model {model_name}')
+
+        return cost / f_star
 
     def assemble_components(self):
         # Initialise assets parameters and variables
@@ -54,36 +92,56 @@ class BuildModel:
 
         # Define objective function
         def obj_function(model):
+            total_objective = 0
+
             # Economic objective
             if self.obj_weights['economic_weight'] > 0:
                 economic_obj = EconomicObjective(model)
 
-                if self.config == CPConfig.CONFIG_1:
-                    investment_cost = economic_obj.investment_cost()
-                else:
-                    investment_cost = economic_obj.investment_cost_linearised()
+                investment_cost = (
+                    economic_obj.investment_cost()
+                    if self.config.value == 'config_1'
+                    else economic_obj.investment_cost_linearised()
+                )
 
-                economic_cost = investment_cost + economic_obj.maintenance_cost() + economic_obj.energy_purchase_cost()
-            else:
-                economic_cost = 0
+                economic_cost = (
+                        investment_cost +
+                        economic_obj.maintenance_cost() +
+                        economic_obj.energy_purchase_cost()
+                )
+
+                economic_cost_norm = self._normalise_cost(economic_cost, 'economic')
+
+                total_objective += self.obj_weights['economic_weight'] * economic_cost_norm
 
             # Technical objective
             if self.obj_weights['technical_weight'] > 0:
                 technical_obj = TechnicalObjective(model)
-                technical_cost = technical_obj.f_disc() + technical_obj.f_peak() + technical_obj.f_papr()
-            else:
-                technical_cost = 0
+
+                technical_cost = (
+                        technical_obj.f_disc() +
+                        technical_obj.f_peak() +
+                        technical_obj.f_papr()
+                )
+
+                technical_cost_norm = self._normalise_cost(technical_cost, 'technical')
+
+                total_objective += self.obj_weights['technical_weight'] * technical_cost_norm
 
             # Social objective
             if self.obj_weights['social_weight'] > 0:
                 social_obj = SocialObjective(model)
-                social_cost = social_obj.f_soc() + social_obj.f_fair()
-            else:
-                social_cost = 0
 
-            return (self.obj_weights['economic_weight'] * economic_cost) + (
-                    self.obj_weights['technical_weight'] * technical_cost) + (
-                    self.obj_weights['social_weight'] * social_cost)
+                social_cost = (
+                        social_obj.f_soc() +
+                        social_obj.f_fair()
+                )
+
+                social_cost_norm = self._normalise_cost(social_cost, 'social')
+
+                total_objective += self.obj_weights['social_weight'] * social_cost_norm
+
+            return total_objective
 
         self.model.obj_function = pyo.Objective(rule=obj_function, sense=pyo.minimize)
 
