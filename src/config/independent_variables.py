ef4bf6a9ea7c113577fc878d97f02c4b22a6aee3
Noah Dimonti <noahdimonti@gmail.com>
2025-06-12 14:06:32 +1000

    update best objective weights based on sensitivity analysis results

--- src/config/independent_variables.py
+++ src/config/independent_variables.py
@@ -1,14 +1,8 @@
 
 tariff_type = 'flat'
 
-# The "best" objective weights so far based on pareto ranks
-obj_weights = {
-    'economic_weight': 0.7,
-    'technical_weight': 0.2,
-    'social_weight': 0.1
-}
 
-obj_weights_from_sensitivity_analysis = {
+obj_weights_dict = {  # for 10 EVs
     'config_1_opportunistic': {
         'economic_weight': 0.7,
         'technical_weight': 0.2,
@@ -19,4 +13,19 @@ obj_weights_from_sensitivity_analysis = {
         'technical_weight': 0.5,
         'social_weight': 0.1
     },
+    'config_2_opportunistic': {
+        'economic_weight': 0.1,
+        'technical_weight': 0.8,
+        'social_weight': 0.1
+    },
+    'config_2_flexible': {
+        'economic_weight': 0.1,
+        'technical_weight': 0.7,
+        'social_weight': 0.2
+    },
+    # 'config_3_opportunistic': {
+    #     'economic_weight': 0,
+    #     'technical_weight': 0,
+    #     'social_weight': 0
+    # },
 }
