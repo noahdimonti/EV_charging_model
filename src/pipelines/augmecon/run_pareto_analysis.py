c64ce8982b93facb3b774a93bc56bce597dfe3d7
Noah Dimonti <u7438524@derlab-noah.der-lab.cecs.anu.edu.au>
2025-09-11 05:07:41 +0000

    Refactor run_pareto_analysis.py

--- src/pipelines/augmecon/run_pareto_analysis.py
+++ src/pipelines/augmecon/run_pareto_analysis.py
@@ -1,52 +1,67 @@
 import pandas as pd
 
 from src.pipelines.augmecon.process_pareto_solutions import get_pareto_solutions
-from src.visualisation.pareto_parallel import pareto_parallel_plot
+from src.visualisation.pareto_front import pareto_parallel_plot
 
 
 def main():
-    config = 'config_2'
-    strategy = 'flexible'
-    grid_points = 10
+    # config = 'config_2'
+    # strategy = 'flexible'
+    # grid_points = 10
 
-    df = get_pareto_solutions(
-        config,
-        strategy,
-        grid_points
-    )
+    versions = [
+        {'config': 'config_1', 'strategy': 'opportunistic', 'grid_points': 20},
+        {'config': 'config_1', 'strategy': 'flexible', 'grid_points': 20},
+        {'config': 'config_2', 'strategy': 'opportunistic', 'grid_points': 20},
+        {'config': 'config_2', 'strategy': 'flexible', 'grid_points': 10},
+        {'config': 'config_3', 'strategy': 'opportunistic', 'grid_points': 20},
+    ]
 
-    # Pareto front only (rank 1)
-    pareto_df = df[df['pareto_rank'] == 1].head(5)
-    pareto_df = pareto_df.round(2)
+    for version in versions:
+        config = version['config']
+        strategy = version['strategy']
+        grid_points = version['grid_points']
 
-    # Best solutions (lowest distance to ideal)
-    best_sols = df[df['distance_to_ideal'] == df['distance_to_ideal'].min()]
-    best_sols_version = best_sols['version'].values
+        df = get_pareto_solutions(
+            config,
+            strategy,
+            grid_points
+        )
 
-    pd.set_option('display.max_columns', None)
-    print('\nFull DataFrame with ranks:')
-    print(df)
+        # Pareto front only (rank 1)
+        num_solution_displayed = 4
+        pareto_df = df[df['pareto_rank'] == 1].head(num_solution_displayed)
+        pareto_df = pareto_df.round(2)
 
-    print('\nPareto front:')
-    print(pareto_df)
+        # Best solutions (lowest distance to ideal)
+        best_sols = df[df['distance_to_ideal'] == df['distance_to_ideal'].min()]
+        best_sols_version = best_sols['version'].values
 
-    print('\nBest solution(s):')
-    print(best_sols)
+        pd.set_option('display.max_columns', None)
+        print('\nFull DataFrame with ranks:')
+        print(df)
 
-    print('\nVersion(s):')
-    print(best_sols_version)
+        print('\nPareto front:')
+        print(pareto_df)
 
-    normalised_df = pareto_df.copy()
-    for col in ['social', 'economic', 'technical']:
-        normalised_df[col] = 1 - (pareto_df[col] - pareto_df[col].min()) / (pareto_df[col].max() - pareto_df[col].min())
+        print('\nBest solution(s):')
+        print(best_sols)
 
-    pareto_parallel_plot(
-        config,
-        strategy,
-        grid_points,
-        # normalised_df
-        pareto_df
-    )
+        print('\nVersion(s):')
+        print(best_sols_version)
+
+        normalised_df = pareto_df.copy()
+        for col in ['social', 'economic', 'technical']:
+            normalised_df[col] = 1 - (pareto_df[col] - pareto_df[col].min()) / (pareto_df[col].max() - pareto_df[col].min())
+
+        pareto_parallel_plot(
+            config,
+            strategy,
+            grid_points,
+            # normalised_df,
+            pareto_df,
+            num_solution_displayed
+        )
 
 
 if __name__ == '__main__':
