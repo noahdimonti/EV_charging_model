e4b21b1314f369b77ca0473a2a517ebd198fde2e
Noah Dimonti <u7438524@derlab-noah.der-lab.cecs.anu.edu.au>
2025-09-17 05:33:20 +0000

    Remove redundant packages

--- src/visualisation/plot_comparison/economic_comparison.py
+++ src/visualisation/plot_comparison/economic_comparison.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 import seaborn as sns
 import os
-from src.config import params, ev_params, independent_variables
+from src.config import params, ev_params
 from src.visualisation import plot_setups
 from src.visualisation import plot_configs
 from pprint import pprint
--- src/visualisation/plot_comparison/technical_comparison.py
+++ src/visualisation/plot_comparison/technical_comparison.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import os
-from src.config import params, ev_params, independent_variables
+from src.config import params, ev_params
 from src.visualisation import plot_setups
 from src.visualisation import plot_configs
 from pprint import pprint
