e8485ff708ddb644fd0b773fd38667082d2952f1
Noah Dimonti <u7438524@derlab-noah.der-lab.cecs.anu.edu.au>
2026-03-29 22:47:03 +0000

    Change target mip gap back to the previous values used in paper

--- src/experiments/run_models.py
+++ src/experiments/run_models.py
@@ -18,16 +18,16 @@ def main():
     # Mapping for mip_gap, time_limit, and verbose for each model
     solver_settings = {  # { model_name: [mip_gap (%), time_limit (minute), verbose, num threads] }
         'config_1_uncoordinated': [None, None, False, 8],
-        'config_1_opportunistic': [None, 120, True, 16],
-        'config_1_flexible': [None, 240, True, 16],
+        'config_1_opportunistic': [1, 120, True, 16],
+        'config_1_flexible': [1, 240, True, 16],
 
         'config_2_uncoordinated': [None, None, False, 8],
-        'config_2_opportunistic': [1, 360, True, 32],
-        'config_2_flexible': [1, 360, True, 32],
+        'config_2_opportunistic': [3, 360, True, 32],
+        'config_2_flexible': [3, 360, True, 32],
 
         'config_3_uncoordinated': [None, None, False, 8],
-        'config_3_opportunistic': [3, 360, True, 32],
-        'config_3_flexible': [3, 900, True, 32],
+        'config_3_opportunistic': [5, 360, True, 32],
+        'config_3_flexible': [5, 720, True, 32],
     }
 
     save_metrics_df = False
